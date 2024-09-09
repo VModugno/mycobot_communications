335e340129d6e5c10e5d5bbbe04e8c045a949c71
MZandtheRaspberryPi <ziegltrum@gmail.com>
2024-09-09 14:31:55 +0100

    adding shell script to setup pi

---
+++ setup_pi/setup_pi.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -eux -o pipefail
+
+WLAN_PASS=$1
+
+sudo apt update
+
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh ./get-docker.sh
+sudo groupadd docker
+sudo usermod -aG docker $USER
+
+sudo apt install network-manager -y
+sudo bash -c "echo 'network: {config: disabled}' > /etc/cloud/cloud.cfg.d/99-disable-network-config.cfg"
+cat 10-my-network-config.yaml | sed 's/REPLACE_PASSWORD/${WLAN_PASS}/g' > /etc/netplan/10-my-config.yaml
+sudo netplan generate
+sudo netplan apply
