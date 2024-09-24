f25b2f93e7025e966b4ee710361b087a6f2d9e7d
MZandtheRaspberryPi <ziegltrum@gmail.com>
2024-09-24 18:14:11 +0100

    trying launch file

---
+++ mycobot_interface_2/launch/mycobot_comms_launch.py
@@ -0,0 +1,32 @@
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch_ros.actions import Node
+from launch.conditions import IfCondition
+from launch.substitutions import LaunchConfiguration, PythonExpression
+
+def generate_launch_description():
+
+    
+    use_realsense = LaunchConfiguration('use_realsense')
+    use_realsense_launch_arg = DeclareLaunchArgument(
+        'use_realsense',
+        default_value='False'
+    )
+    
+    cobot_node = Node(
+            package='mycobot_interface_2',
+            executable='cobot_comms',
+            name='cobot_comms'
+        )
+    
+    realsense_node = Node(
+            package='realsense2_camera',
+            executable='realsense2_camera_node',
+            name='realsense2_camera_node',
+            condition=IfCondition(
+                PythonExpression([
+                    use_realsense])
+                )
+            )
+
+    return LaunchDescription([cobot_node, realsense_node])
