7894c105ba77821763d730c03721311c8e677065
MZandtheRaspberryPi <mzandtheraspberrypi@gmail.com>
2024-10-25 20:19:32 +0100

    adding --rm flag for docker

--- setup_pi/setup_pi_2004.sh
+++ setup_pi/setup_pi_2004.sh
@@ -33,6 +33,6 @@ git clone https://github.com/VModugno/mycobot_client /home/er/ros_ws/src
 
 sudo docker pull mzandtheraspberrypi/mycobot-client-humble:1.2.0
 echo "alias mycobot-up='docker run -it --network host --device /dev/ttyAMA0 --volume /home/er/:/mnt_folder -v /dev:/dev --device-cgroup-rule \"c 81:* rmw\"  --device-cgroup-rule \"c 189:* rmw\" --rm mzandtheraspberrypi/mycobot-ros2:1.1.0'" >>/home/er/.bashrc
-echo "alias mycobot-client-up='docker run -it --network host --volume=/home/er/ros_ws/:/ros_ws --volume /home/er/img_plots:/root/img_plots --workdir /ros_ws mzandtheraspberrypi/mycobot-client-humble:1.2.0'" >>/home/er/.bashrc
+echo "alias mycobot-client-up='docker run -it --network host --volume=/home/er/ros_ws/:/ros_ws --volume /home/er/img_plots:/root/img_plots --workdir /ros_ws --rm mzandtheraspberrypi/mycobot-client-humble:1.2.0'" >>/home/er/.bashrc
 sudo journalctl --vacuum-time=10d # trying to save some disk space
 sudo reboot
