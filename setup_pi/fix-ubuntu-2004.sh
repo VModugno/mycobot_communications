fd36792ebff0ce7ad3e9124369da209d5861fa69
MZandtheRaspberryPi <mzandtheraspberrypi@gmail.com>
2024-10-27 13:42:53 +0000

    setup script to download in folder

--- setup_pi/fix-ubuntu-2004.sh
+++ setup_pi/fix-ubuntu-2004.sh
@@ -3,7 +3,7 @@
 set -eux -o pipefail
 
 mkdir -p /home/er/ros_ws/src
-git clone https://github.com/VModugno/mycobot_client /home/er/ros_ws/src
+git clone https://github.com/VModugno/mycobot_client /home/er/ros_ws/src/mycobot_client
 
 sudo docker pull mzandtheraspberrypi/mycobot-client-humble:1.2.0
 echo "alias mycobot-client-up='docker run -it --network host --volume /dev/shm:/dev/shm --volume=/home/er/ros_ws/:/ros_ws --volume /home/er/img_plots:/root/img_plots --workdir /ros_ws --rm mzandtheraspberrypi/mycobot-client-humble:1.2.0'" >>/home/er/.bashrc
--- setup_pi/setup_pi_2004.sh
+++ setup_pi/setup_pi_2004.sh
@@ -29,7 +29,7 @@ nmcli c modify my-hotspot connection.autoconnect-priority 999
 nmcli c up my-hotspot
 
 mkdir -p /home/er/ros_ws/src
-git clone https://github.com/VModugno/mycobot_client /home/er/ros_ws/src
+git clone https://github.com/VModugno/mycobot_client /home/er/ros_ws/src/mycobot_client
 
 sudo docker pull mzandtheraspberrypi/mycobot-client-humble:1.2.0
 echo "alias mycobot-up='docker run -it --network host --volume /dev/shm:/dev/shm --device /dev/ttyAMA0 --volume /home/er/:/mnt_folder -v /dev:/dev --device-cgroup-rule \"c 81:* rmw\"  --device-cgroup-rule \"c 189:* rmw\" --rm mzandtheraspberrypi/mycobot-ros2:1.1.0'" >>/home/er/.bashrc
