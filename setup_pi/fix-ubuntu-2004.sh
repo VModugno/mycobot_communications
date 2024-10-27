692e1f50ee439d45717cac1f43c548050c9dad50
MZandtheRaspberryPi <mzandtheraspberrypi@gmail.com>
2024-10-27 13:53:33 +0000

    removing reboot for the fix script

--- setup_pi/fix-ubuntu-2004.sh
+++ setup_pi/fix-ubuntu-2004.sh
@@ -7,5 +7,3 @@ git clone https://github.com/VModugno/mycobot_client /home/er/ros_ws/src/mycobot
 
 sudo docker pull mzandtheraspberrypi/mycobot-client-humble:1.2.0
 echo "alias mycobot-client-up='docker run -it --network host --volume /dev/shm:/dev/shm --volume=/home/er/ros_ws/:/ros_ws --volume /home/er/img_plots:/root/img_plots --workdir /ros_ws --rm mzandtheraspberrypi/mycobot-client-humble:1.2.0'" >>/home/er/.bashrc
-sudo journalctl --vacuum-time=10d # trying to save some disk space
-sudo reboot
