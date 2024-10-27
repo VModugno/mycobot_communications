9256695729cbb7e36802edbe89c98fa975b7a8e7
MZandtheRaspberryPi <mzandtheraspberrypi@gmail.com>
2024-10-27 15:09:45 +0000

    fixing the fix script

--- setup_pi/fix-ubuntu-2004.sh
+++ setup_pi/fix-ubuntu-2004.sh
@@ -7,3 +7,5 @@ git clone https://github.com/VModugno/mycobot_client /home/er/ros_ws/src/mycobot
 
 sudo docker pull mzandtheraspberrypi/mycobot-client-humble:1.2.0
 echo "alias mycobot-client-up='docker run -it --network host --volume /dev/shm:/dev/shm --volume=/home/er/ros_ws/:/ros_ws --volume /home/er/img_plots:/root/img_plots --workdir /ros_ws --rm mzandtheraspberrypi/mycobot-client-humble:1.2.0'" >>/home/er/.bashrc
+sed -i 's/.*alias mycobot-up.*/d' /home/er/.bashrc
+echo "alias mycobot-up='docker run -it --network host --volume /dev/shm:/dev/shm --device /dev/ttyAMA0 --volume /home/er/:/mnt_folder -v /dev:/dev --device-cgroup-rule \"c 81:* rmw\"  --device-cgroup-rule \"c 189:* rmw\" --rm mzandtheraspberrypi/mycobot-ros2:1.1.0'" >>/home/er/.bashrc
