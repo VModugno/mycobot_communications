a01fcfbbff12a78cca024385f1a4ffa358c8b56f
MZandtheRaspberryPi <mzandtheraspberrypi@gmail.com>
2024-10-30 17:52:32 +0000

    fixing setup script

--- setup_pi/setup_pi_2004.sh
+++ setup_pi/setup_pi_2004.sh
@@ -19,6 +19,14 @@ sudo sh ./get-docker.sh
 sudo usermod -aG docker $USER
 sudo docker pull mzandtheraspberrypi/mycobot-ros2:1.1.0
 
+mkdir -p /home/er/ros_ws/src
+git clone https://github.com/VModugno/mycobot_client /home/er/ros_ws/src/mycobot_client
+
+sudo docker pull mzandtheraspberrypi/mycobot-client-humble:1.2.0
+echo "alias mycobot-up='docker run -it --network host --volume /dev/shm:/dev/shm --device /dev/ttyAMA0 --volume /home/er/:/mnt_folder -v /dev:/dev --device-cgroup-rule \"c 81:* rmw\"  --device-cgroup-rule \"c 189:* rmw\" --rm mzandtheraspberrypi/mycobot-ros2:1.1.0'" >>/home/er/.bashrc
+echo "alias mycobot-client-up='docker run -it --network host --volume /dev/shm:/dev/shm --volume=/home/er/ros_ws/:/ros_ws --volume /home/er/img_plots:/root/img_plots --workdir /ros_ws --rm mzandtheraspberrypi/mycobot-client-humble:1.2.0'" >>/home/er/.bashrc
+sudo journalctl --vacuum-time=10d # trying to save some disk space
+
 # https://networkmanager.pages.freedesktop.org/NetworkManager/NetworkManager/nm-settings-nmcli.html
 # useful options. We want to setup a wifi network and let folks connect to it and get an ip address
 nmcli d wifi hotspot ifname wlan0 ssid ${WLAN_SSID} password ${WLAN_PASS} con-name my-hotspot
@@ -28,11 +36,4 @@ nmcli c modify my-hotspot connection.autoconnect-priority 999
 
 nmcli c up my-hotspot
 
-mkdir -p /home/er/ros_ws/src
-git clone https://github.com/VModugno/mycobot_client /home/er/ros_ws/src/mycobot_client
-
-sudo docker pull mzandtheraspberrypi/mycobot-client-humble:1.2.0
-echo "alias mycobot-up='docker run -it --network host --volume /dev/shm:/dev/shm --device /dev/ttyAMA0 --volume /home/er/:/mnt_folder -v /dev:/dev --device-cgroup-rule \"c 81:* rmw\"  --device-cgroup-rule \"c 189:* rmw\" --rm mzandtheraspberrypi/mycobot-ros2:1.1.0'" >>/home/er/.bashrc
-echo "alias mycobot-client-up='docker run -it --network host --volume /dev/shm:/dev/shm --volume=/home/er/ros_ws/:/ros_ws --volume /home/er/img_plots:/root/img_plots --workdir /ros_ws --rm mzandtheraspberrypi/mycobot-client-humble:1.2.0'" >>/home/er/.bashrc
-sudo journalctl --vacuum-time=10d # trying to save some disk space
 sudo reboot
